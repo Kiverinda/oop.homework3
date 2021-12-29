288937cfb92ae5898b9dbcdf1f575470b5dd55c4
John Doe <kiverinda@yandex.ru>
2021-12-29 18:46:23 +0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.oop.homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
